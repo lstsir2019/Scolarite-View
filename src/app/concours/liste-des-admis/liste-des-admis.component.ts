6484c5c3f9b762e5a571f40c989de509d5d20f4e
Choubrianas <choubrianas@gmail.com>
2019-04-26 12:10:20 +0100

    bzaaf tl3ibat zinin

---
+++ src/app/concours/liste-des-admis/liste-des-admis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-des-admis',
+  templateUrl: './liste-des-admis.component.html',
+  styleUrls: ['./liste-des-admis.component.css']
+})
+export class ListeDesAdmisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/concours/liste-des-retenus-oral/liste-des-retenus-oral.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-des-retenus-oral',
+  templateUrl: './liste-des-retenus-oral.component.html',
+  styleUrls: ['./liste-des-retenus-oral.component.css']
+})
+export class ListeDesRetenusOralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
